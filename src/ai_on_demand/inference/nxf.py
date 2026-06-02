7f847a2f2089d2dd27bc17803fd3dbad86589c85
sea-shunned <seashunned@gmail.com>
2026-06-02 13:19:58 +0100

    Fix method of getting mask paths to check for reloading

--- src/ai_on_demand/inference/nxf.py
+++ src/ai_on_demand/inference/nxf.py
@@ -695,14 +695,18 @@ Threshold for the Intersection over Union (IoU) metric used in the SAM post-proc
             for layer_name in all_layer_names:
                 if layer_name in self.viewer.layers:
                     self.viewer.layers.remove(layer_name)
-            # Delete masks that match determined layer names (this'll remove partial and full masks, if present)
-            # Restrict the glob to current hash if present for _efficiency_
-            mask_paths = self.mask_dir_path.glob(f"*{parent.run_hash}.rle") if parent.run_hash is not None else self.mask_dir_path.glob("*.rle")
-            for mask_path in mask_paths:
-                for layer_name in all_layer_names:
-                    if layer_name in mask_path.stem:
-                        mask_path.unlink()
-                        break
+            # Delete expected masks to avoid reload
+            # TODO: Switch fully to Nextflow for this, allowing resume to handle reload
+            for img_dict in parent.img_mask_info:
+                expected_mask_fname = parent._get_mask_name(
+                    img_dict["img_path"].stem,
+                    executed=True,
+                    truncate=False,
+                    preprocess_str=img_dict["preprocess_str"],
+                )
+                expected_mask_fpath = self.mask_dir_path / expected_mask_fname
+                if expected_mask_fpath.exists():
+                    expected_mask_fpath.unlink()
         # Check if we already have all the masks
         else:
             proceed, img_paths, load_paths = self.parent.check_masks()
